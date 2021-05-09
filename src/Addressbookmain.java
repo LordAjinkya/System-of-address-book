5b815b4023d8f80c583e2dd5f1240454aad455ba
@LordAjinkya <ajinkyanarale007@gmail.com>
2021-05-09 17:09:00 +0530

    Added:code to add contact

--- src/Addressbookmain.java
+++ src/Addressbookmain.java
@@ -12,8 +12,8 @@ public class Addressbookmain {
         return input;
     }
 
-    //method to get index of Contacts object in addressBook
-    public int indexOfContacts(){
+    //get list of Contacts object in addressBook
+    public int Contactslist(){
         System.out.println();
         String name=readString("Enter First Name : ");
         int index=0;
@@ -25,6 +25,21 @@ public class Addressbookmain {
             ++index;
         }
         return -1;
+    }
+    //created contact in address book
+    public void addContacts(){
+        System.out.println();
+        String firstName=readString("Enter First Name : ");
+        String lastName = readString("Enter Last Name : ");
+        String address=readString("Enter Address : ");
+        String city=readString("Enter City Name : ");
+        String state=readString("Enter State: ");
+        String zipCode=readString("Enter Zip Code : ");
+        String mobileNo=readString("Enter Mobile number: ");
+        String email=readString("Enter email : ");
+        Contacts newEntry=new Contacts(firstName, lastName, city, state, address, email, zipCode, mobileNo);
+        this.data.add(newEntry);
+        System.out.println(firstName+" The Data Added Successfully To The  AddressBook.");
     }
 
 }
